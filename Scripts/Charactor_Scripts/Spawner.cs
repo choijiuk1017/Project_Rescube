6761e97c73bc2d5d78ef726324f618bda483601f
최지욱 <86818323+choijiuk1017@users.noreply.github.com>
2022-07-13 22:30:58 +0900

    Add files via upload

---
+++ Scripts/Charactor_Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public Transform spawnPoint;
+
+    public BetaPlayerMove player;
+    public Monster monster;
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (col.gameObject.tag != "Monster")
+            {
+                StartCoroutine(Spawn());
+            }
+        }
+    }
+
+    //코루틴을 이용해 지속적인 스폰이 가능하도록 설정
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                Instantiate(enemyPrefabs[i], spawnPoint.position, Quaternion.identity);
+            }
+            yield return new WaitForSeconds(10f);
+        }
+    }
+
+
+}
